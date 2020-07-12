ba159d0367543e8a63e7d20bfc543461f707e8da
Muhammad Tabish <mtabishkhanday@gmail.com>
2020-07-12 22:52:16 +0530

    last

--- email.py
+++ email.py
@@ -1,20 +1,28 @@
-import os
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+mail_content='''
+Job 4 for failed, App is not working. Kindly update your code.
+'''
+# The mail address and password
+sender_address = 'mtabishk200@gmail.com'
+sender_pass = 'testing@123'
+reciever_address = 'taabishkhanday@gmail.com'
 
-EMAIL = 'mtabishk200@gmail.com'
-PASSWORD = 'testing@123'
+# setup the MIME
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = reciever_address
+message['Subject'] = 'App is not Working' # subject line
 
-sender = EMAIL
-reciever = 'taabishkhanday@gmail.com'
-passwd = PASSWORD
+# The body and the attachments with the email
+message.attach(MIMEText(mail_content, 'plain'))
 
-server = smtplib.SMTP('smtp.gmail.com',587)
-# Encrypts our traffic
-server.starttls()
-server.login(sender,passwd)
-# Enter message that you want to send
-message = 'Job 4 Build Fail: App is not working'
-# Sends email
-server.sendmail(sender,reciever,message)
-# print send message
-print("Email send successfully")
+# Create SMTP session for sending the mail
+session = smtplib.SMTP('smtp.gmail.com', 587) # using gmail with its port
+session.starttls() # enable security
+session.login(sender_address,sender_pass) # login with the mail id and password
+text = message.as_string()
+session.sendmail(sender_address, reciever_address,text)
+session.quit()
+print('Mail Send Successfully')
